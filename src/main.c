a2c705b052648fee6a3327f6daf8f61488ec9b15
dbarelop <dbarelop@gmail.com>
2016-04-21 16:23:22 +0200

    Replaced tabs by spaces

--- src/main.c
+++ src/main.c
@@ -10,37 +10,38 @@
 
 int main(void)
 {
-	// Configure pin 3 from port F as output and pin 2 as input to use the UART
-	PORTF.DIR = (0x01 << 2);
-	while (1)
-	{
-		blueOsWriteString("Hello world!\n");
-		blueOsDelay(1000);
-	}
+    // Configure pin 3 from port F as output and pin 2 as input to use the UART
+    //PORTF.DIR = (0x01 << 4);
+    PORTF.DIR = 0b00001000;
+    while (1)
+    {
+        blueOsWriteString("Hello world!\n");
+        blueOsDelay(1000);
+    }
 }
 
 int main2(void)
 {
-	int MAX_SPEED = 1000;
-	int i = 0;
-	int speed, step;
+    int MAX_SPEED = 1000;
+    int i = 0;
+    int speed, step;
     while(1)
     {
-		// Every 25 seconds, push the brake, wait for 2 seconds and restart speed
-		if (i == 0)
-		{
-			speed = 0;
-			step = 50;
-			brake();
-			blueOsDelay(2000);
-		}
+        // Every 25 seconds, push the brake, wait for 2 seconds and restart speed
+        if (i == 0)
+        {
+            speed = 0;
+            step = 50;
+            brake();
+            blueOsDelay(2000);
+        }
         accelerate(speed);
-		speed += step;
-		if (speed == MAX_SPEED)
-		{
-			step *= -1;
-		}
-		i = (i + 1) % 1000;
-		blueOsDelay(250);
+        speed += step;
+        if (speed == MAX_SPEED)
+        {
+            step *= -1;
+        }
+        i = (i + 1) % 1000;
+        blueOsDelay(250);
     }
 }
--- src/motor.c
+++ src/motor.c
@@ -9,29 +9,31 @@
 
 void accelerate(int speed)
 {
-	// Disable the brake (CCAEN) and enable the motor (CCBEN)
-	TCD1_CTRLB = TC1_CCBEN_bm;
-	// Configure motor pin (port D, pin 4) as output
-	PORTD.DIR = (0x01 << 3);
-	TCD1_CTRLA = 0x01;					// Prescaler = 1
-	TCD1_CTRLB = TCD1_CTRLB | TC_WGMODE_SS_gc;		// Timer set in Single-slope PWM operation mode
-	TCD1_CTRLC = 0x00;
-	TCD1_CTRLD = 0x00;
-	TCD1_PER = 0xFF;					// Period set to highest possible value
-	// Set the speed
-	TCD1_CCA = TCD1_PER * speed / 1000;	// Compare value set to value equivalent for selected period
+    // Disable the brake (CCAEN) and enable the motor (CCBEN)
+    TCD1_CTRLB = TC1_CCBEN_bm;
+    // Configure motor pin (port D, pin 4) as output
+    //PORTD.DIR = (0x01 << 4);
+    PORTD.DIR = 0b0001000;
+    TCD1_CTRLA = 0x01;                    // Prescaler = 1
+    TCD1_CTRLB = TCD1_CTRLB | TC_WGMODE_SS_gc;        // Timer set in Single-slope PWM operation mode
+    TCD1_CTRLC = 0x00;
+    TCD1_CTRLD = 0x00;
+    TCD1_PER = 0xFF;                    // Period set to highest possible value
+    // Set the speed
+    TCD1_CCA = TCD1_PER * speed / 1000;    // Compare value set to value equivalent for selected period
 }
 
 void brake()
 {
-	// Disable the motor (CCBEN) and enable the brake (CCAEN)
-	TCD1_CTRLB = TC1_CCAEN_bm;
-	// Configure brake pin (port D, pin 5)
-	PORTD.DIR = (0x01 << 4);
-	TCD1_CTRLA = 0x01;				// Prescaler = 1
-	TCD1_CTRLB = TCD1_CTRLB | TC_WGMODE_SS_gc;	// Timer set in Single-slope PWM operation mode
-	TCD1_CTRLC = 0x00;
-	TCD1_CTRLD = 0x00;
-	TCD1_PER = 0xFF;				// Period set to highest possible value
-	TCD1_CCB = 0xFF;				// Compare value set to selected period
+    // Disable the motor (CCBEN) and enable the brake (CCAEN)
+    TCD1_CTRLB = TC1_CCAEN_bm;
+    // Configure brake pin (port D, pin 5)
+    //PORTD.DIR = (0x01 << 5);
+    PORTD.DIR = 0b00100000;
+    TCD1_CTRLA = 0x01;                // Prescaler = 1
+    TCD1_CTRLB = TCD1_CTRLB | TC_WGMODE_SS_gc;    // Timer set in Single-slope PWM operation mode
+    TCD1_CTRLC = 0x00;
+    TCD1_CTRLD = 0x00;
+    TCD1_PER = 0xFF;                // Period set to highest possible value
+    TCD1_CCB = 0xFF;                // Compare value set to selected period
 }
