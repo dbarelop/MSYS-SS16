7f09f5092c43584270b771c32c6d3fffc0b82a92
dbarelop <dbarelop@gmail.com>
2016-06-08 20:13:45 +0200

    Added position algorithm to PID controller (floating point)

--- src/pid.c
+++ src/pid.c
@@ -14,6 +14,37 @@ static volatile unsigned int target_speed = 0;
 
 void pid()
 {
+	pid_diff();
+}
+
+/*
+ * Implementation of the PID position algorithm.
+ */
+void pid_pos()
+{
+	// TODO: implement using fixed point
+	const float q0 = Kp + Ki * Ta + Kd / Ta, 
+				q1 = -Kp - 2 * Kd / Ta, 
+				q2 = Kd / Ta;
+	float output, err, prev_err = 0, prev_err2 = 0;
+
+	while (1)
+	{
+		err = target_speed - getCurrentSpeed();
+
+		output += q0 * err + q1 * prev_err + q2 * prev_err2;
+
+		prev_err2 = prev_err;
+		prev_err = err;
+		blueOsDelay(250);
+	}
+}
+
+/*
+ * Implementation of the PID differential algorithm.
+ */
+void pid_diff()
+{
 	// TODO: implement using fixed point
 	float output, err, ierr = 0, derr, prev_err = 0;
 	
@@ -22,12 +53,12 @@ void pid()
 		err = target_speed - getCurrentSpeed();
 		ierr += err;
 		derr = err - prev_err;
-		prev_err = err;
 
 		output = (Kp * err) + (Ki * Ta * ierr) + (Kd * derr / Ta);
 
 		accelerate(output);
 
+		prev_err = err;
 		blueOsDelay(250);
 	}
 }
